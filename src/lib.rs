33aaeaaf61d86b3259dbe5cd48bdcf1f367c3f2a
Adrien Tremblay <adrien@adrientremblay.com>
2020-11-11 13:44:56 -0500

    fill search func to pass test

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,15 @@ pub fn run(cfg: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    vec![]
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
 }
 
 pub struct Config {
@@ -36,10 +44,10 @@ mod tests {
     fn one_result() {
         let query = "duct";
         let contents = "\
-        Rust:
-        safe, fast, productive.
-        Pick three.";
+Rust:
+safe, fast, productive.
+Pick three.";
 
-        assert_eq!(vec!["safe, fast, productive"], search(query, contents));
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 }
