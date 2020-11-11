58a32698de30e6df335fe0026746b582455d5f12
Adrien Tremblay <adrien@adrientremblay.com>
2020-11-11 15:44:24 -0500

    standard error

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,12 @@ use std::process;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let cfg = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
     if let Err(e) = ch12::run(cfg) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
