90b0f7ed3c4735bbe04b8a3e8d07b7a31764c9c6
Adrien Tremblay <adrien@adrientremblay.com>
2020-11-11 01:29:35 -0500

    library

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
+use ch12::Config;
 use std::env;
-use std::error::Error;
-use std::fs;
 use std::process;
 
 fn main() {
@@ -13,32 +12,8 @@ fn main() {
     println!("Searching for word '{}'.", cfg.query);
     println!("In file '{}'.", cfg.filename);
 
-    if let Err(e) = run(cfg) {
+    if let Err(e) = ch12::run(cfg) {
         println!("Application error: {}", e);
         process::exit(1);
-    }
-}
-
-fn run(cfg: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(cfg.filename)?;
-    println!("With text:\n{}", contents);
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-        Ok(Config { query, filename })
     }
 }
